dca375be5624b7f66730f47d9fb7dcb2a26bc949
chandu712 <chandanaramesh712@gmail.com>
2024-06-18 17:54:46 -0400

    mysql

--- mysql.sh
+++ mysql.sh
@@ -13,6 +13,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
+echo "Please enter DB password: "
+read -s mysql_root_password
 if [ $USERID -ne 0 ]
 then
     echo "Please run this script with root access"
@@ -43,8 +46,26 @@ VALIDATE $? "Enabling mysql server"
 systemctl start mysqld  &>>LOGFILE
 VALIDATE $? "Starting mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1  &>>LOGFILE
-VALIDATE $? "Setting up root password"
+# mysql_secure_installation --set-root-pass ExpenseApp@1  &>>LOGFILE
+# VALIDATE $? "Setting up root password"
+
+#Above lines of code are not idempotenet in nature.So, we need to do some changes to achieve idempotency nature.
+
+
+
+mysql -h db.chandudevops.online -uroot -p${mysql_root_password}  -e 'SHOW DATABASES;" &>>$LOGFILE
+
+if [ $? -ne 0 ]
+then 
+    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+
+    # Here we are hardcoding the password in code, it is not a good practice. Instead we can pass values during runtime by using read command
+    VALIDATE $? "MYsql root password setup".
+    # We need to enter correct password. otherwise backend(Nodejs) will fail, because the same password has setup for the backend
+else
+    echo -e "MYSQL Root password is already setup .... $Y SKIPPING $N"
+    exit 1
+fi
 
 
 
