df35e3894354a428a3361dad8db68c50b2e3a4b4
chandu712 <chandanaramesh712@gmail.com>
2024-06-18 20:11:17 -0400

    backend

--- backend.sh
+++ backend.sh
@@ -43,31 +43,31 @@ dnf module enable nodejs:20 -y &>>LOGFILE
 VALIDATE $? "Enabling default nodejs"
 
 dnf install nodejs -y &>>LOGFILE
-VALIDATE $? "Disabling default nodejs" 
+VALIDATE $? "Installing nodejs" 
 
 
 id expense &>>LOGFILE
 if [ $? -ne 0 ]
 then
     useradd expense &>>LOGFILE
-    VALIDATE $? "Creating expense user" 
+    VALIDATE $? "Creating expense user to run application" 
 else
     echo -e "Expense user is already created ....$Y SKIPPING $N"
     exit 1
 fi
 
 # if app directory is not created it will create one. This command is idempotenet
-mkdir -p /app
+mkdir -p /app &>>LOGFILE
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>LOGFILE
 VALIDATE $? "Downloading the backend code"
 
 cd /app
-unzip /tmp/backend.zip
+unzip /tmp/backend.zip &>>LOGFILE
 VALIDATE $? "Extracted backend code"
 
-npm install
+npm install &>>LOGFILE
 VALIDATE $? "Installing NodeJS dependencies"
 
 # Humans can use vim editor, shell script doesn't understand vim
@@ -75,6 +75,32 @@ VALIDATE $? "Installing NodeJS dependencies"
 # Create a backend.service file in the same Expense project folder, copy the source file(give absolute path) to 
 # destination folder(/etc/systemd/service)
 # Absolute path of backend.service is /home/ec2-user/Expense-shell/backend.service
+
+cp /home/ec2-user/Expense-shell/backend.service /etc/systemd/system/backend.service &>>LOGFILE
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>LOGFILE
+VALIDATE $? "Daemon reload"
+
+systemctl start backend &>>LOGFILE
+VALIDATE $? "Starting backend"
+
+systemctl enable backend &>>LOGFILE
+VALIDATE $? "Enabling backend"
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "Installing MySQL client"
+
+mysql -h db.chandudevops.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>LOGFILE
+VALIDATE $? "Schema Loading"
+
+systemctl restart backend &>>LOGFILE
+VALIDATE $? "Restarting backend"
+
+
+
+
+
 
 
 
